c34e2e801f6705019fa987698757281664418388
James Wilson <draconicsapience@gmail.com>
2025-12-10 16:15:23 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == "ADS2025":
+        if st.session_state["password"] == st.secrets["password"]:
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store password
         else:
